f13544a2b10607e6d6bd30b53386980fc0bbdc2f
panda zheng <pandazheng@pandas-MacBook-Pro.local>
2013-04-28 04:28:03 -0700

    Initial Commit

---
+++ Pickers/BIDAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  BIDAppDelegate.h
+//  Pickers
+//
+//  Created by panda zheng on 13-4-28.
+//  Copyright (c) 2013年 panda zheng. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface BIDAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ Pickers/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  Pickers
+//
+//  Created by panda zheng on 13-4-28.
+//  Copyright (c) 2013年 panda zheng. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "BIDAppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([BIDAppDelegate class]));
+    }
+}
